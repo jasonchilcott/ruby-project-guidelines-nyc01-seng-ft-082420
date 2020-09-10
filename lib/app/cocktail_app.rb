00551632f160170e3e9c039beb95b1816c38f964
jdhofmann <justindhofmann@gmail.com>
2020-09-10 14:12:51 -0400

    resolving merge issues in cocktail_app

--- lib/app/cocktail_app.rb
+++ lib/app/cocktail_app.rb
@@ -16,66 +16,125 @@ class CocktailApp
 
   $prompt = TTY::Prompt.new
 
-def welcome
-  puts "Welcome to the Cocktail App"
-  sleep(1.5)
-end
+  def welcome
+    puts "Welcome to the Cocktail App"
+    sleep(1.5)
+  end
 
-def log_in_or_sign_up
-  choices = ['log in', 'sign up']
-  action = $prompt.multi_select("What would you like to do?", choices, required: true, min: 1, max: 1)
-    if action == ['sign up']
-      sign_up
-    elsif action == ['log in']
+  def log_in_or_sign_up
+    choices = ['log in', 'sign up']
+    action = $prompt.multi_select("What would you like to do?", choices, required: true, min: 1, max: 1)
+      if action == ['sign up']
+        sign_up
+      elsif action == ['log in']
+      else
+        abort "Bye"
+      end
+  end
+    
+  def sign_up 
+    puts 'signing up'
+    new_user = $prompt.collect do
+      key(:name).ask("Name:", required: true)
+      key(:email_address).ask("Email:", required: true){ |q| q.validate :email }
+      key(:password).mask("Password:", required: true)
+    end
+    User.create(name: new_user[:name], email: new_user[:email_address], password: new_user[:password])
+    puts "Thanks for creating an account #{new_user[:name]} \nPlease log in"
+  end
+
+  def validate_password
+    count = 0
+    while count < 3
+      validate_pw = $prompt.mask('Password', required: true)
+      if validate_pw != @user.password
+        puts "That is not a valid password"
+        count += 1
+      else return @user
+      end
+    end
+    @user = nil
+    abort "Goodbye"
+  end
+
+  def log_in 
+    find_user = $prompt.ask("Name:", required: true)
+
+    if User.find_by(name: find_user)
+      @user = User.find_by(name: find_user)
+      validate_password
     else
-      abort "Bye"
+      sleep(0.5)
+      puts "We don't have you on file, please sign up"
+      sleep(0.5)
+      sign_up
+      log_in
     end
-end
+  end
+
+  def main_menu
+    # drinks = GetDrinks.new
+    # puts "have random #{drinks.get_drink_name} on us..."
+    choices = ['Search for drinks', 'Add your own drink', 'Favorites']
+    main_menu_response = $prompt.multi_select("Alright #{@user.name}, what's next", choices, required: true, max: 1)
+    if main_menu_response == ['Search for drinks']
+      puts "Let's go find you a drink!"
+    elsif main_menu_response == ['Add your own drink']
+      puts "Let's add your own creation!"
+    elsif main_menu_response == ['Favorites']
+      puts "Let's checkout your favorites"
+    end
+    # multiple choice question here: max: 1
+    # 1: Find a drink by indgredient(s)
+    # 2: Find a drink by ...
+    # 3: Add your own drink
+    # 4: Favorites
+
+  end
   
-def sign_up 
-  puts 'signing up'
-  new_user = $prompt.collect do
-    key(:name).ask("Name:", required: true)
-    key(:email_address).ask("Email:", required: true){ |q| q.validate :email }
-    key(:password).mask("Password:", required: true)
+  def find_drink_by_ingredients
+    # "Please enter an igredient"
+    # would you like to enter another (repeat until they say no)
+    
+    ## unsure how to structure api call here ##
+    ## how many ingredients can we search by - can we get the api to do all the work here? ###
+    
+    #builds an array of 10 drinks
+
+    #calls drink_names_menu(array_of_10_drinks)
+
+    
+    #############might need this code #############
+    #  new_ingredient = GetDrinks.new(ingredient)
+    #  new_ingredient
   end
-  User.create(name: new_user[:name], email: new_user[:email_address], password: new_user[:password])
-  puts "Thanks for creating an account #{new_user[:name]} \nPlease log in"
-end
+  
+  def drink_names_menu(drinks)
+    # turns drinks array into menu options
 
-def validate_password
-  count = 0
-  while count < 3
-    validate_pw = $prompt.mask('Password', required: true)
-    if validate_pw != @user.password
-      puts "That is not a valid password"
-      count += 1
-    else return @user
-    end
+    
+    # calls find_drink_by_name()
   end
-  @user = nil
-  abort "Goodbye"
-end
-
-def log_in 
-  find_user = $prompt.ask("Name:", required: true)
-
-  if User.find_by(name: find_user)
-    @user = User.find_by(name: find_user)
-    validate_password
-  else
-    sleep(0.5)
-    puts "We don't have you on file, please sign up"
-    sleep(0.5)
-    sign_up
-    log_in
+  
+  def find_drink_by_name
+    # displays drink
+    # find_drink = GetDrinks.new('drink_name')
+    # find_drink.get_drink_with_instructions
+    # with question prompt at the bottom to return to drink array or main menu
   end
-end
 
-def main_menu
-  puts "Alright #{@user.name}"
-  drinks = GetDrinks.new
-  puts "have random #{drinks.get_drink_name} on us..."
-end
-
+  def add_user_drink
+    # question prompts in order
+      # 1. add Drink name ( 
+        # new_drink = Drink.create(name:'name', user_id: @user.id, alcoholic: true, instructions: nil)
+        # UserDrink.create(user_id: @user.id, drink_id: new_drink.id)
+        # )
+      # 2. Is drink alcoholic (new_drink.alcoholic = 'response')
+      # 3. add ingredient multiple input question
+        # add ingredient (Ingredient.find_or_create() save ID as a variable)
+        # add measurement ()
+      # 4. repeat add ingredient or move forward
+      # 5. add instructions ( new_drink.instructions = 'response')
+      # returns user to main_menu
+  end
 end
